dd62e8268b9d1177f246baa2e1c83fba2d6c0531
RodrigoRagna <rodrigo.rivera@ciencias.unam.mx>
2015-10-10 01:15:07 -0500

    Update Practica4.rkt

--- Practicas/Practica4/Practica4.rkt
+++ Practicas/Practica4/Practica4.rkt
@@ -17,3 +17,26 @@
     [funS (params p) (fun params (desugar p))]
     [appS (a lst) (app (desugar a) (map desugar lst))]
     [binopS (b l r) (binop b (desugar l) (desugar r))]))
+
+
+;;Ejercicio 3
+
+(define (opbin op d e)
+  (cond
+    [(numV (op (numV d) (numV e)))]))
+(define (lookup b env)
+  (env b))
+
+;;Ejercicio 4
+
+(define (interp expr env)
+  (type-case FAE expr
+    [num (a) (numV a)]
+    [id (b) (lookup b env)]
+    [fun (params body) (closureV params body env)]
+    [app (fun args) '()] ;Falta
+    [binop (c d e) (numV (c (numV (interp d env)) (numV (interp e env))))]
+    ))
+
+(define (rinterp expr)
+  (interp expr (mtSub)))
